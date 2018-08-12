34745d9587bd07081cb02e22961d1c0bed6653dd
Devin Rhode <devinrhode2@gmail.com>
2018-08-12 14:16:31 -0500

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         capital one auto deal finder
-// @description  Reload once you are on the /inventory page! rounds price, mileage, and replaces carfax link with mileage/dollar value. Lowest means best deal!
+// @description  Reload once you are on the /inventory page! Enter `runCapOneAutoScript()` in console when you change pages. rounds price, mileage, and replaces carfax link with mileage/dollar value. Lowest means best deal!
 // @namespace    https://openuserjs.org/users/devinrhode2
 // @copyright    2018, devinrhode2 (https://openuserjs.org/users/devinrhode2)
 // @license      MIT
